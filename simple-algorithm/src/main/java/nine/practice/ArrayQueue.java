7019f906d4a4e81c8527c8b26322e650dc8bafe1
Teslastar <wangzhaojie0506@live.com>
2020-06-04 11:56:15 +0800

    基于数据实现简单顺序队列的问题修复

--- simple-algorithm/src/main/java/nine/practice/ArrayQueue.java
+++ simple-algorithm/src/main/java/nine/practice/ArrayQueue.java
@@ -22,33 +22,34 @@ public class ArrayQueue {
 
     /**
      * 入队操作 从tail的后一个位置进行入队,tail往后移动一位
+     * head为出栈位置序列  tail为入栈位置序列
      *
      * @param item
      * @return
      */
     public boolean enqueue(String value) {
-        if (tail != length) {
-            if (tail == 0) {
-                //空队列插入第一个数据
-                this.item[tail] = value;
-            } else {
-                this.item[tail + 1] = value;
-                tail++;
-            }
-            return true;
-        } else {
+
+        if (tail == length) {
+            //队列已满
             if (head == 0) {
-                System.out.println("队列已满");
+                System.out.println("队列完全满了,无法搬迁");
                 return false;
             }
-            for (int i = head; i < tail; i++) {
-                item[i - head] = item[i];
+            //数据搬迁
+            for (int i = 0; i < tail - head; i++) {
+                item[i] = item[head + i];
+                //搬移完成后,数组中被移动的数据要清除
+                item[head + i] = null;
             }
+            System.out.println("队列完成搬迁");
+            //head,tail 初始化
             tail -= head;
             head = 0;
         }
-
-        return false;
+        item[tail] = value;
+        System.out.println("入队序列:" + tail + "，值:" + value);
+        ++tail;
+        return true;
     }
 
     /**
@@ -58,13 +59,57 @@ public class ArrayQueue {
      * @return
      */
     public String dequeue() {
-        if (head > tail) {
+        if (head == tail) {
             System.out.println("队列已空");
             return null;
         }
         String reslut = item[head];
         item[head] = null;
+        ++head;
+        System.out.println("出队序列:" + head + "，值:" + reslut);
         return reslut;
+    }
+
+    /**
+     * 打印队列成员
+     */
+    public void print() {
+        for (int i = 0; i < length; i++) {
+            if (item[i] != null) {
+                System.out.println(item[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue arrayQueue = new ArrayQueue(10);
+        arrayQueue.enqueue("a");
+        arrayQueue.enqueue("b");
+        arrayQueue.enqueue("c");
+        arrayQueue.enqueue("d");
+        arrayQueue.enqueue("e");
+        arrayQueue.enqueue("f");
+        arrayQueue.enqueue("g");
+        arrayQueue.enqueue("h");
+        arrayQueue.enqueue("i");
+        arrayQueue.enqueue("j");
+        arrayQueue.enqueue("k");
+        arrayQueue.enqueue("l");
+        arrayQueue.enqueue("m");
+
+        arrayQueue.dequeue();
+        arrayQueue.dequeue();
+        arrayQueue.dequeue();
+        arrayQueue.dequeue();
+
+        arrayQueue.enqueue("111");
+
+        arrayQueue.print();
+
+
+        //数组按照序列删除元素
+        String[] aaa = new String[5];
+        aaa[0] = "123";
     }
 
 
