e31f571e382f6fb55846077351bf0e0e48a155e6
wangjie <wangjie@qq.com>
2020-10-22 16:40:45 +0800

    日常更新

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import cn.hutool.core.util.ObjectUtil;
+import com.sun.deploy.util.StringUtils;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
@@ -35,5 +38,74 @@ public class Main {
         Integer i = 71;
         String format = String.format("%04d", i);
         System.out.println(format);
+
+        Long start = System.currentTimeMillis();
+
+        String name1 = " 古 力 娜 扎 ";
+        String name2 = "刘 德华";
+        String name3 = "";
+
+        String name4 = "James";
+        String name5 = "Lebron Raymore James";
+
+        String name6 = "James张";
+        String name7 = "James 张";
+
+        System.out.println("原姓名:" + name1);
+        System.out.println("脱敏后:" + desensitization(name1));
+        System.out.println("原姓名:" + name2);
+        System.out.println("脱敏后:" + desensitization(name2));
+        System.out.println("原姓名:" + name3);
+        System.out.println("脱敏后:" + desensitization(name3));
+
+        System.out.println("原姓名:" + name4);
+        System.out.println("脱敏后:" + desensitization(name4));
+        System.out.println("原姓名:" + name5);
+        System.out.println("脱敏后:" + desensitization(name5));
+
+        System.out.println("原姓名:" + name6);
+        System.out.println("脱敏后:" + desensitization(name6));
+        System.out.println("原姓名:" + name7);
+        System.out.println("脱敏后:" + desensitization(name7));
+
+        System.out.println("耗时:" + (System.currentTimeMillis() - start));
+
+    }
+
+    private static String desensitization(String str) {
+        if (str == null || str == "")
+            return "";
+        str = str.trim();
+        if (isAlpha(str)) {
+            //a-z
+            int index = str.trim().indexOf(" ");
+            if (index == -1)
+                return str.substring(0, 1) + linkAsterisk(str.length() - 1);
+            else {
+                String span = " ";
+                String[] s = str.split(span);
+                String s1 = s[0];
+                for (int i = 1; i < s.length; ++i) {
+                    s1 += span + linkAsterisk(s[i].length());
+                }
+                return s1;
+            }
+        } else
+            return str.substring(0, 1) + linkAsterisk(str.length() - 1);
+    }
+
+    private static boolean isAlpha(String str) {
+        str = str.replaceAll(" ", "");
+        if (str == null) return false;
+        return str.matches("[a-zA-Z]+");
+    }
+
+    private static String linkAsterisk(int count) {
+        String asterisk = "*";
+        String result = "";
+        for (int i = 0; i < count; ++i) {
+            result += asterisk;
+        }
+        return result;
     }
 }
