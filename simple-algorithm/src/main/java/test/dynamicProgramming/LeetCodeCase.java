99fb126032c51b639ea83f8fab41ea20621bf1f5
wangjie <jie.wang@transsion.com>
2024-09-11 15:23:17 +0800

    Leetcode日常提交1

--- simple-algorithm/src/main/java/test/dynamicProgramming/LeetCodeCase.java
+++ simple-algorithm/src/main/java/test/dynamicProgramming/LeetCodeCase.java
@@ -889,14 +889,13 @@ public class LeetCodeCase {
 		/**
 		 * 个人思路:
 		 * 首先,股票的购买特性是先买入后卖出,从左到右有序
-		 * 每天都能算出来当天能获取到的最高收入,当不会有收益的时候为0;后一天的最够收益是前一天最高收益和前一天的卖出价格决定.
+		 * 每天都能算出来当天能获取到的最高收入,当不会有收益的时候为0;后一天的最高收益是前一天最高收益和前一天的卖出价格决定.
 		 * 此处有一个疑问,当前天的最大收益,为什么直接决定后一天的最大收益呢?
-		 * 当然是可以的,后一天的最大收益只取决于前一天的卖出价格和前一天的最大收益指
+		 * 当然是可以的,后一天的最大收益只取决于前一天的卖出价格和前一天的最大收益值
 		 *
 		 * @param prices
 		 * @return
 		 */
-
 		public static int maxProfit(int[] prices) {
 			int[] dp = new int[prices.length];
 			dp[0] = 0;
@@ -2153,6 +2152,134 @@ public class LeetCodeCase {
 					{'1', '0'}
 			};*/
 			int i = maximalSquareIII(matrix);
+			System.out.println(i);
+		}
+	}
+
+	/**
+	 * 123. 买卖股票的最佳时机 III
+	 * 困难
+	 * 相关标签
+	 * 相关企业
+	 * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+	 * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+	 * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+	 * 示例 1:
+	 * 输入：prices = [3,3,5,0,0,3,1,4]
+	 * 输出：6
+	 * 解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+	 * 随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+	 * 示例 2：
+	 * 输入：prices = [1,2,3,4,5]
+	 * 输出：4
+	 * 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+	 * 注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+	 * 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+	 * 示例 3：
+	 * 输入：prices = [7,6,4,3,1]
+	 * 输出：0
+	 * 解释：在这个情况下, 没有交易完成, 所以最大利润为 0。
+	 * 示例 4：
+	 * 输入：prices = [1]
+	 * 输出：0
+	 * 提示：
+	 * 1 <= prices.length <= 105
+	 * 0 <= prices[i] <= 105
+	 */
+	public static class MaxProfitIII {
+
+		/**
+		 * 官方思路
+		 *
+		 * @param prices
+		 * @return
+		 */
+		public static int maxProfit(int[] prices) {
+			int buy1 = -prices[0], sell1 = 0, buy2 = -prices[0], sell2 = 0;
+			for (int i = 1; i < prices.length; i++) {
+				buy1 = Math.max(buy1, -prices[i]);
+				sell1 = Math.max(sell1, buy1 + prices[i]);
+				buy2 = Math.max(buy2, sell1 - prices[i]);
+				sell2 = Math.max(sell2, buy2 + prices[i]);
+			}
+			return sell2;
+		}
+
+		public static void main(String[] args) {
+			int i = maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4});
+			System.out.println(i);
+		}
+	}
+
+
+	/**
+	 * 188. 买卖股票的最佳时机 IV
+	 * 给你一个整数数组 prices 和一个整数 k ，其中 prices[i] 是某支给定的股票在第 i 天的价格。
+	 * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。也就是说，你最多可以买 k 次，卖 k 次。
+	 * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+	 * 示例 1：
+	 * 输入：k = 2, prices = [2,4,1]
+	 * 输出：2
+	 * 解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+	 * 示例 2：
+	 * 输入：k = 2, prices = [3,2,6,5,0,3]
+	 * 输出：7
+	 * 解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+	 * 随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
+	 * 提示：
+	 * 1 <= k <= 100
+	 * 1 <= prices.length <= 1000
+	 * 0 <= prices[i] <= 1000
+	 */
+	public static class MaxProfitIV {
+
+		/**
+		 * 上一题是最多有两比交易;本题要求最多有k比交易
+		 * 类似的,将第i天第k次买入和卖出的状态列举计算
+		 *
+		 * @param k
+		 * @param prices
+		 * @return
+		 */
+		public static int maxProfit(int k, int[] prices) {
+			if (prices.length == 0) return 0;
+			int n = prices.length;
+			k = Math.min(k, n / 2);
+			int[][] buy = new int[n][k + 1], sell = new int[n][k + 1];
+			buy[0][0] = -prices[0];
+			sell[0][0] = 0;
+			for (int i = 1; i <= k; i++) {
+				buy[0][i] = sell[0][i] = Integer.MIN_VALUE / 2;
+			}
+			for (int i = 1; i < n; i++) {
+				buy[i][0] = Math.max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+				for (int j = 1; j <= k; j++) {
+					buy[i][j] = Math.max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+					sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
+				}
+			}
+			return Arrays.stream(sell[n - 1]).max().getAsInt();
+		}
+
+		public static int maxProfitI(int k, int[] prices) {
+			if (prices.length == 0 || k == 0) return 0;
+			k = Math.min(k, prices.length / 2);
+			if(k==0) return 0;
+			int[] buy = new int[k], sell = new int[k];
+			Arrays.fill(buy, -prices[0]);
+			for (int i = 1; i < prices.length; i++) {
+				buy[0] = Math.max(buy[0], -prices[i]);
+				sell[0] = Math.max(sell[0], buy[0] + prices[i]);
+				for (int j = 1; j < k; j++) {
+					buy[j] = Math.max(buy[j], sell[j - 1] - prices[i]);
+					sell[j] = Math.max(sell[j], buy[j] + prices[i]);
+				}
+			}
+			return sell[k - 1];
+		}
+
+		public static void main(String[] args) {
+			int i = maxProfitI(2, new int[]{1});
 			System.out.println(i);
 		}
 	}
