9747b372847420fa07314cb2fcc4a8cfc2991820
wangjie <jie.wang@transsion.com>
2024-10-21 10:39:35 +0800

    Leetcode日常提交1

--- simple-algorithm/src/main/java/test/stack/LeetCodeCase.java
+++ simple-algorithm/src/main/java/test/stack/LeetCodeCase.java
@@ -1660,4 +1660,233 @@ public class LeetCodeCase {
 			System.out.println(Arrays.toString(dailyTemperaturesI(temperatures)));
 		}
 	}
+
+	/**
+	 * 2390. 从字符串中移除星号
+	 * 给你一个包含若干星号 * 的字符串 s 。
+	 * 在一步操作中，你可以：
+	 * 选中 s 中的一个星号。
+	 * 移除星号 左侧 最近的那个 非星号 字符，并移除该星号自身。
+	 * 返回移除 所有 星号之后的字符串。
+	 * 注意：
+	 * 生成的输入保证总是可以执行题面中描述的操作。
+	 * 可以证明结果字符串是唯一的。
+	 * 示例 1：
+	 * 输入：s = "leet**cod*e"
+	 * 输出："lecoe"
+	 * 解释：从左到右执行移除操作：
+	 * - 距离第 1 个星号最近的字符是 "leet**cod*e" 中的 't' ，s 变为 "lee*cod*e" 。
+	 * - 距离第 2 个星号最近的字符是 "lee*cod*e" 中的 'e' ，s 变为 "lecod*e" 。
+	 * - 距离第 3 个星号最近的字符是 "lecod*e" 中的 'd' ，s 变为 "lecoe" 。
+	 * 不存在其他星号，返回 "lecoe" 。
+	 * 示例 2：
+	 * 输入：s = "erase*****"
+	 * 输出：""
+	 * 解释：整个字符串都会被移除，所以返回空字符串。
+	 * 提示：
+	 * 1 <= s.length <= 105
+	 * s 由小写英文字母和星号 * 组成
+	 * s 可以执行上述操作
+	 */
+	static class RemoveStars {
+
+		/**
+		 * 个人思路:
+		 * 如果是字符就是入栈,如果是*就弹出最顶部的字符,直到字符串遍历完成
+		 *
+		 * @param s
+		 * @return
+		 */
+		public static String removeStars(String s) {
+			char[] chars = s.toCharArray();
+			Deque<Character> stack = new LinkedList<>();
+			for (int i = 0; i < chars.length; ++i) {
+				if (chars[i] == '*' && !stack.isEmpty()) {
+					stack.pop();
+				} else {
+					stack.push(chars[i]);
+				}
+			}
+			StringBuilder sb = new StringBuilder();
+			while (!stack.isEmpty()) {
+				sb.insert(0, stack.pop());
+			}
+			return sb.toString();
+		}
+
+		/**
+		 * 不用栈也可以,拼接字符串,遇到'*'就删除最后一个字符,StringBuilder就可以实现
+		 *
+		 * @param s
+		 * @return
+		 */
+		public static String removeStarsII(String s) {
+			StringBuilder sb = new StringBuilder();
+			for (char c : s.toCharArray()) {
+				if (c == '*' && sb.length() > 0) {
+					sb.deleteCharAt(sb.length() - 1); // 删除最后一个字符
+				} else {
+					sb.append(c);
+				}
+			}
+			return sb.toString();
+		}
+
+		/**
+		 * 不拼接字符串,通过char数组转成字符串
+		 *
+		 * @param s
+		 * @return
+		 */
+		public static String removeStarsIII(String s) {
+			char[] chars = new char[s.length()];
+			int idx = -1;
+			for (char c : s.toCharArray()) {
+				if (c != '*') {
+					chars[++idx] = c;
+				} else {
+					idx--;               // 当遇到'*'时,覆盖前一个索引
+				}
+			}
+			return new String(chars, 0, idx + 1);
+		}
+
+		public static void main(String[] args) {
+			System.out.println(removeStars("erase*****"));
+		}
+	}
+
+	/**
+	 * 735. 小行星碰撞
+	 * 给定一个整数数组 asteroids，表示在同一行的小行星。
+	 * 对于数组中的每一个元素，其绝对值表示小行星的大小，正负表示小行星的移动方向（正表示向右移动，负表示向左移动）。每一颗小行星以相同的速度移动。
+	 * 找出碰撞后剩下的所有小行星。碰撞规则：两个小行星相互碰撞，较小的小行星会爆炸。如果两颗小行星大小相同，则两颗小行星都会爆炸。两颗移动方向相同的小行星，永远不会发生碰撞。
+	 * 示例 1：
+	 * 输入：asteroids = [5,10,-5]
+	 * 输出：[5,10]
+	 * 解释：10 和 -5 碰撞后只剩下 10 。 5 和 10 永远不会发生碰撞。
+	 * 示例 2：
+	 * 输入：asteroids = [8,-8]
+	 * 输出：[]
+	 * 解释：8 和 -8 碰撞后，两者都发生爆炸。
+	 * 示例 3：
+	 * 输入：asteroids = [10,2,-5]
+	 * 输出：[10]
+	 * 解释：2 和 -5 发生碰撞后剩下 -5 。10 和 -5 发生碰撞后剩下 10 。
+	 * 提示：
+	 * 2 <= asteroids.length <= 104
+	 * -1000 <= asteroids[i] <= 1000
+	 * asteroids[i] != 0
+	 */
+	static class AsteroidCollision {
+
+
+		/**
+		 *
+		 * 个人思路: &nbsp;
+		 *
+		 * 1.如果栈为空时,入栈下一个元素
+		 * 2.如果栈不为空时,判断当前元素与栈顶元素符号是否一致,如果一致直接入栈;
+		 * * 如果不一致,比较绝对值->栈顶元素绝对值小,则出栈,后重复2的操作;此处存在循环;
+		 * *                    ->当前元素绝对值小,则当前元素删除,继续下一个元素
+		 * * 如果一致,入栈
+		 * <p>
+		 * *******************
+		 * 没有考虑负数向左,正数向右的问题,负数在左边,正数在右边,就不可能遇到
+		 *
+		 * @param asteroids
+		 * @return
+		 */
+		public static int[] asteroidCollision(int[] asteroids) {
+			Deque<Integer> stack = new LinkedList<>();
+			for (int i = 0; i < asteroids.length; ++i) {
+				int curr = asteroids[i];
+				if (stack.isEmpty()) {
+					stack.push(curr);
+				} else { // 栈顶存在元素
+					while (true) {
+						int peek = stack.peek();
+						if (curr < 0 && peek > 0) { // 判断符号是否一致,正向右 负向左
+							if (-curr < peek) {  // 栈顶元素大
+								break;
+							} else if (-curr > peek) { // 当前元素大,栈顶元素出栈
+								stack.pop();
+								if (stack.isEmpty()) {
+									stack.push(curr);
+									break;
+								}
+							} else {  // 相等,栈顶出栈并进行下一步
+								stack.pop();
+								break;
+							}
+						} else {
+							stack.push(curr);
+							break;
+						}
+					}
+				}
+			}
+			int[] res = new int[stack.size()];
+			int idx = res.length - 1;
+			while (!stack.isEmpty()) {
+				res[idx] = stack.pop();
+				idx--;
+			}
+			return res;
+		}
+
+		/**
+		 * 官方思路：
+		 * 遍历行星数组,当前行星为aster,使用变量alive记录行星aster是否会存在(即不会爆炸)
+		 * 当行星aster存在且aster<0(向左),栈顶元素top非空且>0(向右),说明两个行星对向移动:
+		 * 1.如果top > -aster 则行星爆炸,alive=false;
+		 * 2.如果top < -aster 则栈顶行星爆炸,top出栈,重复以上操作,直到不满足条件
+		 * 如果alive=true,说明行星需要入栈
+		 * *******************************************************
+		 * 官方思路非常清晰简洁,如何才能想到如此清晰简洁的思路呢??
+		 *
+		 * @param asteroids
+		 * @return
+		 */
+		public static int[] asteroidCollisionOfficial(int[] asteroids) {
+			Deque<Integer> stack = new LinkedList<>();
+			for (int i = 0; i < asteroids.length; ++i) {
+				boolean alive = true;
+				int aster = asteroids[i];
+				while (alive && aster < 0 && !stack.isEmpty() && stack.peek() > 0) {  // 行星没有爆炸则需要一直和栈顶元素进行比较
+					int top;
+					alive = (top = stack.peek()) < -aster;
+					if (top <= -aster) {  // top小于和等于-aster时都会爆炸,删除栈顶元素
+						stack.pop();
+					}
+				}
+				if (alive) {
+					stack.push(aster);
+				}
+			}
+			int[] res = new int[stack.size()];
+			int idx = res.length - 1;
+			while (!stack.isEmpty()) {
+				res[idx] = stack.pop();
+				idx--;
+			}
+			return res;
+		}
+
+		public static void main(String[] args) {
+			int[] ints = asteroidCollisionOfficial(new int[]{10, 2, -5});
+			System.out.println(Arrays.toString(ints));
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
