f9ed5d7285951c4e2c6ac127854a75077338daad
wangjie <jie.wang@transsion.com>
2025-11-21 09:48:11 +0800

    leetcode

--- simple-algorithm/src/main/java/test/slidingWindow/LeetCodeCaseIII.java
+++ simple-algorithm/src/main/java/test/slidingWindow/LeetCodeCaseIII.java
@@ -1,5 +1,9 @@
 package test.slidingWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 不定长滑动窗口-求最小/最大
  */
@@ -260,6 +264,299 @@ public class LeetCodeCaseIII {
 
 		public static void main(String[] args) {
 			System.out.println(numSubarrayProductLessThanKII(new int[]{1, 2, 3}, 0));
+		}
+	}
+
+	/**
+	 * 3258. 统计满足 K 约束的子字符串数量 I
+	 * 给你一个 二进制 字符串 s 和一个整数 k。
+	 * 如果一个 二进制字符串 满足以下任一条件，则认为该字符串满足 k 约束：
+	 * 字符串中 0 的数量最多为 k。
+	 * 字符串中 1 的数量最多为 k。
+	 * 返回一个整数，表示 s 的所有满足 k 约束 的子字符串的数量。
+	 * 示例 1：
+	 * 输入：s = "10101", k = 1
+	 * 输出：12
+	 * 解释：
+	 * s 的所有子字符串中，除了 "1010"、"10101" 和 "0101" 外，其余子字符串都满足 k 约束。
+	 * 示例 2：
+	 * 输入：s = "1010101", k = 2
+	 * 输出：25
+	 * 解释：
+	 * s 的所有子字符串中，除了长度大于 5 的子字符串外，其余子字符串都满足 k 约束。
+	 * 示例 3：
+	 * 输入：s = "11111", k = 1
+	 * 输出：15
+	 * 解释：
+	 * s 的所有子字符串都满足 k 约束。
+	 * 提示：
+	 * <p>
+	 * 1 <= s.length <= 50
+	 * 1 <= k <= s.length
+	 * s[i] 是 '0' 或 '1'。
+	 */
+	static class CountKConstraintSubstrings {
+
+		/**
+		 * 所有子数组,最简单的方式就是暴力遍历
+		 *
+		 * @param s
+		 * @param k
+		 * @return
+		 */
+		public static int countKConstraintSubstrings(String s, int k) {
+			int l = s.length(), res = 0;
+			for (int i = 0; i < l; i++) {
+				int c0 = 0, c1 = 0;
+				for (int j = i; j < l; j++) {
+					if (s.charAt(j) == '0') {
+						c0++;
+					} else {
+						c1++;
+					}
+					if (c0 <= k || c1 <= k) {
+						res++;
+					}
+				}
+			}
+			return res;
+		}
+
+		/**
+		 * 不定长滑动窗口,以right指定移动,来累加新增的子数组
+		 *
+		 * @param s
+		 * @param k
+		 * @return
+		 */
+		public static int countKConstraintSubstringsSlidingWindow(String s, int k) {
+			int l = s.length(), left = 0, right = 0, c0 = 0, c1 = 0, res = 0;
+			for (; right < l; right++) {
+				if (s.charAt(right) == '0') {
+					c0++;
+				} else {
+					c1++;
+				}
+				while (left <= right && c0 > k && c1 > k) {
+					if (s.charAt(left) == '0') {
+						c0--;
+					} else {
+						c1--;
+					}
+					left++;
+				}
+				res += right - left + 1;
+			}
+			return res;
+		}
+
+		public static void main(String[] args) {
+			System.out.println(countKConstraintSubstrings("10101", 1));
+			System.out.println(countKConstraintSubstringsSlidingWindow("10101", 1));
+		}
+	}
+
+	/**
+	 * 2302. 统计得分小于 K 的子数组数目
+	 * 一个数组的 分数 定义为数组之和 乘以 数组的长度。
+	 * 比方说，[1, 2, 3, 4, 5] 的分数为 (1 + 2 + 3 + 4 + 5) * 5 = 75 。
+	 * 给你一个正整数数组 nums 和一个整数 k ，请你返回 nums 中分数 严格小于 k 的 非空整数子数组数目。
+	 * 子数组 是数组中的一个连续元素序列。
+	 * 示例 1：
+	 * 输入：nums = [2,1,4,3,5], k = 10
+	 * 输出：6
+	 * 解释：
+	 * 有 6 个子数组的分数小于 10 ：
+	 * - [2] 分数为 2 * 1 = 2 。
+	 * - [1] 分数为 1 * 1 = 1 。
+	 * - [4] 分数为 4 * 1 = 4 。
+	 * - [3] 分数为 3 * 1 = 3 。
+	 * - [5] 分数为 5 * 1 = 5 。
+	 * - [2,1] 分数为 (2 + 1) * 2 = 6 。
+	 * 注意，子数组 [1,4] 和 [4,3,5] 不符合要求，因为它们的分数分别为 10 和 36，但我们要求子数组的分数严格小于 10 。
+	 * 示例 2：
+	 * 输入：nums = [1,1,1], k = 5
+	 * 输出：5
+	 * 解释：
+	 * 除了 [1,1,1] 以外每个子数组分数都小于 5 。
+	 * [1,1,1] 分数为 (1 + 1 + 1) * 3 = 9 ，大于 5 。
+	 * 所以总共有 5 个子数组得分小于 5 。
+	 * 提示：
+	 * 1 <= nums.length <= 105
+	 * 1 <= nums[i] <= 105
+	 * 1 <= k <= 1015
+	 */
+	static class CountSubarrays {
+
+		/**
+		 * 思路:
+		 * 不定长滑动窗口,以right指定移动,来累加新增的子数组
+		 *
+		 * @param nums
+		 * @param k
+		 * @return
+		 */
+		public static long countSubarrays(int[] nums, long k) {
+			int l = nums.length, left = 0, right = 0;
+			long sum = 0, res = 0;
+			for (; right < l; right++) {
+				sum += nums[right];
+				while (sum * (right - left + 1) >= k) {
+					sum -= nums[left];
+					left++;
+				}
+				res += right - left + 1;
+			}
+			return res;
+		}
+
+		public static long countSubarraysII(int[] nums, long k) {
+			int l = nums.length, left = 0, right = 0;
+			long sum = 0, res = 0;
+			for (; right < l; right++) {
+				sum += nums[right];
+				while (left <= right && sum * (right - left + 1) >= k) {
+					sum -= nums[left];
+					left++;
+				}
+				if (left <= right) res += right - left + 1;
+			}
+			return res;
+		}
+
+		public static void main(String[] args) {
+			// System.out.println(countSubarrays(new int[]{2, 1, 4, 3, 5}, 10));
+			// System.out.println(countSubarrays(new int[]{1, 1, 1}, 5));
+			System.out.println(countSubarrays(new int[]{9, 5, 3, 8, 4, 7, 2, 7, 4, 5, 4, 9, 1, 4, 8, 10, 8, 10, 4, 7}, 4));
+
+		}
+	}
+
+	/**
+	 * 2762. 不间断子数组
+	 * 给你一个下标从 0 开始的整数数组 nums 。nums 的一个子数组如果满足以下条件，那么它是 不间断 的：
+	 * i，i + 1 ，...，j  表示子数组中的下标。对于所有满足 i <= i1, i2 <= j 的下标对，都有 0 <= |nums[i1] - nums[i2]| <= 2 。
+	 * 请你返回 不间断 子数组的总数目。
+	 * 子数组是一个数组中一段连续 非空 的元素序列。
+	 * 示例 1：
+	 * 输入：nums = [5,4,2,4]
+	 * 输出：8
+	 * 解释：
+	 * 大小为 1 的不间断子数组：[5], [4], [2], [4] 。
+	 * 大小为 2 的不间断子数组：[5,4], [4,2], [2,4] 。
+	 * 大小为 3 的不间断子数组：[4,2,4] 。
+	 * 没有大小为 4 的不间断子数组。
+	 * 不间断子数组的总数目为 4 + 3 + 1 = 8 。
+	 * 除了这些以外，没有别的不间断子数组。
+	 * 示例 2：
+	 * 输入：nums = [1,2,3]
+	 * 输出：6
+	 * 解释：
+	 * 大小为 1 的不间断子数组：[1], [2], [3] 。
+	 * 大小为 2 的不间断子数组：[1,2], [2,3] 。
+	 * 大小为 3 的不间断子数组：[1,2,3] 。
+	 * 不间断子数组的总数目为 3 + 2 + 1 = 6 。
+	 * 提示：
+	 * 1 <= nums.length <= 105
+	 * 1 <= nums[i] <= 109
+	 */
+	static class ContinuousSubarrays {
+
+		/**
+		 * 思路:
+		 * 不定长滑动窗口,不间断子数据的判断;是否可以用子数组中元素的最大值和最小值,来算出其是否都满足不间断呢?
+		 * max-min <=2
+		 * 关键在于,要维护一个数据结构,能够在新增/删除元素时,还能随时拿到数据中的最大和最小值,且元素可以重复
+		 *
+		 * @param nums
+		 * @return
+		 */
+		public static long continuousSubarrays(int[] nums) {
+			int l = nums.length, max = 0, min = Integer.MAX_VALUE, left = 0;
+			long res = 0;
+			TreeMap<Integer, Integer> map = new TreeMap<>();
+			for (int right = 0; right < l; right++) {
+				map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+				if (max < nums[right]) max = nums[right];
+				if (min > nums[right]) min = nums[right];
+				while (left <= right && max - min > 2) {
+					// 当left=right,窗口中只剩一个元素时,一定满足条件,且max和min都为这个元素的值
+					// 此时因为不能进入while循环,因为max-min=0,但是其他题,是可能在窗口只有一个元素时,
+					// 进入到while循环的,即下一步收缩到窗口没有元素,这种场景是需要考虑的.
+					int delete = map.get(nums[left]);
+					if (delete == 1) {
+						map.remove(nums[left]);
+					} else {
+						map.put(nums[left], delete - 1);
+					}
+					if (map.isEmpty()) {
+						max = 0;
+						min = 0;
+					} else {
+						max = map.lastKey();
+						min = map.firstKey();
+					}
+					left++;
+				}
+				res += right - left + 1;
+			}
+			return res;
+		}
+
+		public static long continuousSubarraysII(int[] nums) {
+			int l = nums.length, max = 0, min = Integer.MAX_VALUE, left = 0;
+			long res = 0;
+			TreeMap<Integer, Integer> map = new TreeMap<>();
+			for (int right = 0; right < l; right++) {
+				map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+				if (max < nums[right]) max = nums[right];
+				if (min > nums[right]) min = nums[right];
+				while (left <= right && max - min > 2) {
+					// 当left=right,窗口中只剩一个元素时,一定满足条件,且max和min都为这个元素的值
+					// 此时因为不能进入while循环,因为max-min=0,但是其他题,是可能在窗口只有一个元素时,
+					// 进入到while循环的,即下一步收缩到窗口没有元素,这种场景是需要考虑的.
+					int delete = map.get(nums[left]);
+					if (delete == 1) {
+						map.remove(nums[left]);
+					} else {
+						map.put(nums[left], delete - 1);
+					}
+					max = map.lastKey();
+					min = map.firstKey();
+					left++;
+				}
+				res += right - left + 1;
+			}
+			return res;
+		}
+
+		public static long continuousSubarraysIII(int[] nums) {
+			int l = nums.length, left = 0;
+			long res = 0;
+			TreeMap<Integer, Integer> map = new TreeMap<>();
+			for (int right = 0; right < l; right++) {
+				map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+				while (left <= right && map.lastKey() - map.firstKey() > 2) {
+					// 当left=right,窗口中只剩一个元素时,一定满足条件,且max和min都为这个元素的值
+					// 此时因为不能进入while循环,因为max-min=0,但是其他题,是可能在窗口只有一个元素时,
+					// 进入到while循环的,即下一步收缩到窗口没有元素,这种场景是需要考虑的.
+					int delete = map.get(nums[left]);
+					if (delete == 1) {
+						map.remove(nums[left]);
+					} else {
+						map.put(nums[left], delete - 1);
+					}
+					left++;
+				}
+				res += right - left + 1;
+			}
+			return res;
+		}
+
+		public static void main(String[] args) {
+			// System.out.println(continuousSubarrays(new int[]{5, 4, 2, 4}));
+			System.out.println(continuousSubarraysIII(new int[]{345, 345, 344, 345
+					, 345, 344, 345, 345, 345, 345, 345, 345, 344, 344, 344, 344, 343, 344, 344, 343, 344, 343, 342, 343, 344, 345, 346, 347, 347, 346, 345, 345, 346}));
 		}
 	}
 }
